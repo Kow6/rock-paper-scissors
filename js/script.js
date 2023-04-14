eda741bfc0293c672400b8d88f393044ed38efd6
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-04-14 15:03:24 -0500

    Fix funtion to get player choice

--- js/script.js
+++ js/script.js
@@ -7,42 +7,65 @@ store computer selection in r/p/s - DONE
 compare 2 selections
 output the answer
 
-
-
-
 */
 
+// Declare some varibales
+
 let computerChoice
+let playerChoice
+
+// Create Funtion for Computer Choice of R/P/S
 
 function getComputerChoice(){
     let computerChoiceVariable = Math.random()
     
     if (computerChoiceVariable < .334){
-        computerChoice = "Rock"
+        computerChoice = "rock"
     }
 
     else if (computerChoiceVariable <.667){
-        computerChoice ="Paper"
+        computerChoice ="paper"
     }
 
     else
-        computerChoice ="Scissors"
-    
-       
+        computerChoice ="scissors"
+          
         
     return computerChoice
 }
 
+//Create and store player Choice of R/P/S
 
 
-let playerChoice = prompt("Enter your selection")
+function getPlayerChoice(){
+
+playerChoice = prompt("Enter your selection")
 
 playerChoice.toLowerCase
 
-if (playerChoice != "rock" || playerChoice != "paper" || playerChoice != "scissors") {
+if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
+    console.log("great")
+}
+    else getPlayerChoice()
 
-    playerChoice = prompt("Renter your selection")}
+}
 
-console.log(playerChoice)
+getPlayerChoice()
 
+/* function gameRound(){
 
+    if (playerChoice == "rock" && computerChoice == "rock"){
+        console.log("tie")
+    }
+
+    else if (playerChoice == "rock" && computerChoice == "paper") {
+        console.log("You Lose! Paper covers Rock")
+    }
+    else if (playerChoice == "rock" && computerChoice == "scissors") {
+        console.log("You Win! Rock crushes Scissors")
+        }
+    else
+        console.log("not coded")
+    }
+
+gameRound */
