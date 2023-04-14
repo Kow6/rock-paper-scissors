1eb58b726824dc6bbf6b0d9aa852b5fb5b50d9b3
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-04-14 14:35:59 -0500

    Add prompt to capture player chouice variable

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* 
 generate computer r/p/s - DONE
-create selection of r/p/s
-capture input of r/p/s
-store input of r/p/s into variable
-store computer selection in r/p/s
+create selection of r/p/s - DONE
+capture input of r/p/s - DONE
+store input of r/p/s into variable - DONE
+store computer selection in r/p/s - DONE
 compare 2 selections
 output the answer
+
+
 
 
 */
 
 let computerChoice
+
 function getComputerChoice(){
     let computerChoiceVariable = Math.random()
     
@@ -30,4 +33,16 @@ function getComputerChoice(){
     return computerChoice
 }
 
-console.log(getComputerChoice())
+
+
+let playerChoice = prompt("Enter your selection")
+
+playerChoice.toLowerCase
+
+if (playerChoice != "rock" || playerChoice != "paper" || playerChoice != "scissors") {
+
+    playerChoice = prompt("Renter your selection")}
+
+console.log(playerChoice)
+
+
